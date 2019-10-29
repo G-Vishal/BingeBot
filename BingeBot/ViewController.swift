2e1bf46b3f228f365166071803a7a573225b0c19
Vishal Shelake <922.vishal@gmail.com>
2019-10-29 17:04:24 +0530

    Udmey Choose Randm String and shows on Screen

--- BingeBot/ViewController.swift
+++ BingeBot/ViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showsList: UILabel!
+    @IBOutlet weak var stackFirst: UIStackView!
+    @IBOutlet weak var stackSecond: UIStackView!
+    @IBOutlet weak var randomChoice: UILabel!
+    @IBOutlet weak var bingebotText: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    var shows : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        stackFirst.isHidden = true
+        stackSecond.isHidden = true
     }
-
-
+    
+    @IBAction func addToShowsBtnTapped(_ sender: Any) {
+        
+        guard  let showName = textField.text else {
+            return
+        }
+        
+        shows.append(showName)
+        stackFirst.isHidden = false
+        updateShowLabel()
+        textField.text = ""
+        
+        if shows.count  > 1 {
+            stackSecond.isHidden = false
+            bingebotText.isHidden = true
+            randomChoice.isHidden = true
+        }
+    }
+    
+    func updateShowLabel() {
+        
+        guard textField.text!.isEmpty else {
+            return showsList.text = shows.joined(separator: ", ")
+        }
+        
+        //there is still a problem in showing the extra ,(commas) on array to Print on the array fond Solction for this
+     
+    }
+    
+    @IBAction func whatToBingeBtnTapped(_ sender: Any) {
+        randomChoice.text = shows.randomElement()
+        bingebotText.isHidden = false
+        randomChoice.isHidden  = false
+        randomChoice.text = shows.randomElement()
+        
+    }
+ 
 }
 
